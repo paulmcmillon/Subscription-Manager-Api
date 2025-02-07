664378ee51300c6bf41aa3eaa0ab5c12537f34db
Paul McMillon <paul.mcmillon@softeknology.com>
2025-02-07 12:30:17 -0600

    Added design time EF DbContext

---
+++ Subscriptions.Data/SubscriptionsContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Subscriptions.Data
+{
+    internal class SubscriptionsContextFactory : IDesignTimeDbContextFactory<SubscriptionsContext>
+    {
+        public SubscriptionsContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<SubscriptionsContext>();
+            optionsBuilder.UseSqlServer("Server=tcp:softeknology-sql-server.database.windows.net,1433;Initial Catalog=subscription-manager;Persist Security Info=False;User ID=softeknology-sql-admin;Password=Pmcmillon1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            //optionsBuilder.UseSqlServer("Data Source=LAPTOP-5NBB4HF0;Initial Catalog=subscription-manager;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+            return new SubscriptionsContext(optionsBuilder.Options);
+        }
+    }
+}
