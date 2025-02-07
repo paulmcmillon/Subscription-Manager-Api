f4d1a42ae301140741bfe4d9c6aef7d43c8a06d2
Paul McMillon <paul.mcmillon@softeknology.com>
2025-02-07 17:19:09 -0600

    Added class to return secret from Azure Key Vault

--- Subscriptions.Api/Program.cs
+++ Subscriptions.Api/Program.cs
@@ -1,9 +1,17 @@
 using Azure.Identity;
 using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.Runtime.CompilerServices;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri")!);
+var keyVaultEndpoint = new Uri(builder.Configuration["AzureKeyVault:Vault"]);
+var secretName = builder.Configuration["AzureKeyVault:SecretName"];
 builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+
+var connectionString= builder.Configuration[secretName];
 
 builder.Services.AddAzureClients(clientBuilder =>
 {
@@ -12,7 +20,9 @@ builder.Services.AddAzureClients(clientBuilder =>
     clientBuilder.AddTableServiceClient(builder.Configuration["StorageConnection:tableServiceUri"]!).WithName("StorageConnection");
 });
 
-string sqlConnectionString = builder.Configuration["SqlConnection:connectionString"]!;
+// NOTE: This will pull from the UserSecrets file...might want to use this as a backup
+// in case call to KeyVault fails or for local development.
+string sqlConnectionString = builder.Configuration["SqlConnection:ConnectionString"]!;
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ Subscriptions.Infrastructure/Subscriptions.Infrastructure/Azure/KeyVault.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace Subscriptions.Infrastructure.Azure
+{
+    internal sealed class KeyVault(string vaultUri)
+    {
+        private readonly SecretClient _secretClient = new(new Uri(vaultUri), new DefaultAzureCredential());
+
+        public string GetSecret(string secretName)
+        {
+            return _secretClient.GetSecret(secretName).Value.Value;
+        }
+    }
+}
