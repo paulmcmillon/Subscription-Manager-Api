b5fa389bdbf675ba35f41a0fa65e7d3aba698499
Paul McMillon <paul.mcmillon@softeknology.com>
2025-02-09 07:06:06 -0600

    Added setup for Azure services, CORS configuration and Azure SQL dBContext connection to Programs.cs. closes #9

--- Subscriptions.Api/Program.cs
+++ Subscriptions.Api/Program.cs
@@ -1,17 +1,59 @@
 using Azure.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Azure;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using System.Runtime.CompilerServices;
-
+using Subscriptions.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultEndpoint = new Uri(builder.Configuration["AzureKeyVault:Vault"]);
-var secretName = builder.Configuration["AzureKeyVault:SecretName"];
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+// Setup CORS policy to allow requests from the specified origin (clients)
+builder.Services.AddCors(policy =>
+{
+    policy.AddPolicy("DefaultPolicy", opt => opt
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .SetIsOriginAllowed(origin => true)
+        .WithOrigins("https://localhost:7210/"));
+});
+
+// Get the Azure Key Vault endpoint from the configuration
+var keyVaultEndpointUri = builder.Configuration["AzureKeyVault:Vault"];
+if (string.IsNullOrEmpty(keyVaultEndpointUri))
+{
+    throw new ArgumentNullException(nameof(keyVaultEndpointUri), "Azure Key Vault endpoint is not configured.");
+}
+var keyVaultEndpoint = new Uri(keyVaultEndpointUri);
+// Add Azure Key Vault service to the container.
 builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
-var connectionString= builder.Configuration[secretName];
+// Get the secret name to lookup from the configuration
+var secretName = builder.Configuration["AzureKeyVault:SecretName"];
+if (string.IsNullOrEmpty(secretName))
+{
+    // TODO: Log this error
+    throw new ArgumentNullException(nameof(secretName), "Secret name is not configured.");
+}
+// Get the SQL connection string from Azure Key Vault
+var sqlConnectionString = builder.Configuration[secretName];
+if (string.IsNullOrEmpty(sqlConnectionString))
+{
+    // TODO: Log this error
+    throw new ArgumentNullException(nameof(sqlConnectionString), "SQL connection string is not configured.");
+}
+builder.Services.AddDbContext<SubscriptionsContext>(options =>
+{
+    options.UseSqlServer(sqlConnectionString,
+    options => options.EnableRetryOnFailure());
+});
+
+// TODO: Create passwordless connection to Azure SQL
+
+// NOTE: This will pull from the UserSecrets file...might want to use this as a backup
+// in case call to KeyVault fails or for local development.
+string localConnectionString = builder.Configuration["LocalServices:localConnectionString"]!;
 
 builder.Services.AddAzureClients(clientBuilder =>
 {
@@ -20,15 +62,8 @@ builder.Services.AddAzureClients(clientBuilder =>
     clientBuilder.AddTableServiceClient(builder.Configuration["StorageConnection:tableServiceUri"]!).WithName("StorageConnection");
 });
 
-// NOTE: This will pull from the UserSecrets file...might want to use this as a backup
-// in case call to KeyVault fails or for local development.
-string sqlConnectionString = builder.Configuration["SqlConnection:ConnectionString"]!;
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
+app.UseCors("DefaultPolicy");
 
 if (app.Environment.IsDevelopment())
 {
@@ -37,6 +72,4 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-
 app.Run();
